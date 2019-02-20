ebabf392d72b4637c0998020897b2186d7e16407
twelveD <ddxy399@outlook.com>
2019-02-20 19:42:38 +0800

    -

--- PAT_Basic/1092.cpp
+++ PAT_Basic/1092.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int a[1005] = {0};
 int main() {
 	int kind, city, num;
 	cin >> kind >> city;
 	for (int i = 0; i < city; i++) {
-		//几个城市遍历
-		for (int j = 0; j < kind; j++) {
-			//几个种类挨个	
+		for (int j = 0; j < kind; j++) {	
 			cin >> num;
 			a[j] += num; 
 		}
-	}
-	//以上输入结束，开始比较大小
+	}	//以上输入结束，开始比较大小
 	int max = -1;
 	int flag = 0;
 	for (int i = 0; i < kind; i++) 
@@ -24,7 +20,6 @@ int main() {
 			if (flag) cout << " ";
 			cout << i+1;
 			flag = 1;
-		}
-		
+		}		
 	return 0;
 }

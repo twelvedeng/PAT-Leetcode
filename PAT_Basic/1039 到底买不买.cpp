5e18f8621fc9a04426449f62f60ceacd475e4ef6
twelveD <ddxy399@outlook.com>
2019-02-24 22:22:46 +0800

    beating bug

--- PAT_Basic/1039 到底买不买.cpp
+++ PAT_Basic/1039 到底买不买.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-char a[70] = {0};
-char b[70] = {0};
+int a[256];
+int b[256];
 int main() {
 	string give, hong;
-	getline(cin, give);
-	getline(cin, hong);
+	cin >> give >> hong;
 	int count = 0;
 	for (int i = 0; i < hong.size(); i++) a[hong[i]]++; 
-	for (int j = 0; j < give.size(); j++) b[give[j]]++;
+	for (int i = 0; i < give.size(); i++) b[give[j]]++;
 	for (int i = 0; i < hong.size(); i++) 
 		if (a[hong[i]] <= b[hong[i]]){
 			count += a[hong[i]];
 			b[hong[i]] = 0;
+		//	cout << count;
 		}
 	count == hong.size() ? cout << "Yes" << " " << give.size()-hong.size(): cout << "No" << " " << hong.size()-count-1; 
 	return 0;

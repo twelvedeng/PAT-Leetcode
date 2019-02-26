9345441f25af75b4cab036caf24df228ed75bdaf
twelveD <ddxy399@outlook.com>
2019-02-26 20:22:34 +0800

    部分正确13

---
+++ PAT_Basic/1038 统计同成绩学生.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int a[101] = {0};
+int main() {
+	int n, grade, m, search;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> grade;
+		a[grade]++;
+	}
+	cin >> m;
+	int flag = 0;
+	for (int i = 0; i < m; i++) {
+		cin >> search;
+		if (flag) cout << " ";
+		cout << a[search];
+		flag = 1;
+	}
+	return 0;
+}

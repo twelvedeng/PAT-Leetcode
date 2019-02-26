33993ddcd8ae6ae06738ed4e212da0c26b1bd3ec
twelveD <ddxy399@outlook.com>
2019-02-26 22:09:23 +0800

    -

--- PAT_Basic/1038 统计同成绩学生.cpp
+++ PAT_Basic/1038 统计同成绩学生.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int a[101] = {0};
+//int a[101] = {0};
 int main() {
 	int n, grade, m, search;
 	cin >> n;
+	vector<int> a(n);
 	for (int i = 0; i < n; i++) {
 		cin >> grade;
 		a[grade]++;

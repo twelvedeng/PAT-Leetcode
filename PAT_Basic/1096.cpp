0d7f26e29da9d021d26e1f89cd720a4afe9ffe88
twelveD <ddxy399@outlook.com>
2019-03-06 22:58:11 +0800

    -

--- PAT_Basic/1096.cpp
+++ PAT_Basic/1096.cpp
@@ -5,19 +5,26 @@
 using namespace std;
 //int num[10000] = {0};
 vector<int> v;
-void its_yin(int a) {
-	for (int i = 1; i <= floor(sqrt(a) + 0.5); i++){
-		if (a % i == 0) {
+void its_yin(int x) {
+	for (int i = 1; i <= floor(sqrt(x) + 0.5); i++){
+		if (x % i == 0) {
 			v.push_back(i);
-			if (a/i != a)
-				v.push_back(a/i); 
+			if (x/i != x)
+				v.push_back(x/i); 
 		}
 	}
+	int flag = 0;
 	if (v.size() < 4) cout << "No\n";
 	else {
-		for (int i = 0; i < v.size(); i++) {
-			
-		}	
+		for (int a = 0; a < v.size() && !flag; a++) 
+			for(int b = a+1; b < v.size() && !flag; b++)
+				for(int c = b+1; c < v.size() && !flag; c++)
+					for (int d = c+1; d < v.size() && !flag; d++)
+						if (x % (v.at(a)+v.at(b)+v.at(c)+v.at(d))) {
+							cout << "Yes\n";
+							flag = 1;
+						}
+		if (!flag) cout << "No\n";
 	}
 }
 int main() {

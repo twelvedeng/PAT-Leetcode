464b858c74e5a60c51d9788f90fe1e390e01e013
twelveD <ddxy399@outlook.com>
2019-03-23 18:31:13 +0800

    -

--- PAT_Advanced/1133 Splitting A Linked List.cpp
+++ PAT_Advanced/1133 Splitting A Linked List.cpp
@@ -24,16 +24,17 @@ int main() {
 		x = list[x].next;
 	}
 	int cnt = 0;
-	int i, j;
-	for(i = 0; i < 3; i++) {
-		for(j = 0; j < v[i].size(); j++) {
+	//int i, j;
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < v[i].size(); j++) {
 			if(cnt != n-1) {
 				printf("%05d %d %05d\n", v[i][j], list[v[i][j]].data, j+1==v[i].size() ? v[i+1][0] : v[i][j+1]);
 				cnt++;
-			}else{break;}
+			}else{printf("%05d %d -1\n", v[i][j], list[v[i][j]].data); break;}
 		}
 		if(cnt == n-1) break;
 	} 
-	printf("%05d %d -1\n", v[i][j], list[v[i][j]].data);
+	
 	return 0;
 }
+21` 

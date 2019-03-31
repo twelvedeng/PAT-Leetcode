0de5f8933433836c0944344e800733b641f5c6d5
twelveD <ddxy399@outlook.com>
2019-04-01 00:38:57 +0800

    -

---
+++ PAT_Basic/1071 小赌怡情.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int gt, k, n1, b, t, n2;
+	scanf("%d%d", &gt, &k);
+	for(int i = 0; i < k; i++) {
+		int flag = 0;
+		scanf("%d%d%d%d", &n1, &b, &t, &n2);
+		if(t > gt && gt != 0) {
+			cout << "Not enough tokens.  Total = " << gt << ".\n";
+			flag = 1; 
+		}
+		if(flag) continue;
+		if(gt == 0) {cout << "Game Over.\n"; break;}
+		if((b == 0 && n1 > n2)||(b == 1 && n1 < n2))
+			{gt+=t; printf("Win %d!  ", t);} 
+		else
+			{gt-=t; printf("Lose %d.  ", t);} 
+		printf("Total = %d.\n", gt);
+	}
+}

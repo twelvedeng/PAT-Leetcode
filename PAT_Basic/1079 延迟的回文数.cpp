2409e8583670db9f54477be6daadef8423cbe5d1
twelveD <ddxy399@outlook.com>
2019-04-01 13:12:40 +0800

    -

--- PAT_Basic/1079 延迟的回文数.cpp
+++ PAT_Basic/1079 延迟的回文数.cpp
@@ -1,25 +1,27 @@
+/*fix error£ºa, b may beyond int*/ 
+#include <cstdio>
 #include <iostream>	
 #include <string>
 #include <algorithm>
 using namespace std;
 int main() {
-	string s, t;
+	string s, t, sum;
 	int cnt = 0;
 	cin >> s;
 	t = s;
 	reverse(s.begin(), s.end());
-	getchar();
 	while (s != t && cnt < 10)	{
-		int a = stoi(t), b = stoi(s);
-	//	cout << t << " " << "+" << " " << s;
-		printf(" = %d\n",a+b);
-		s = to_string(a+b);
+		
+		/*int a = stoi(t), b = stoi(s);
+		cout << t << " " << "+" << " " << s;
+		printf(" = %d\n", a+b);
+		s = to_string(a+b);*/
 		t = s;
 		reverse(s.begin(), s.end());
 		cnt++;
 	}
 	if (s == t) {cout << s;	printf(" is a palindromic number.\n");}
-	else
+	else if(cnt == 10)
 		printf("Not found in 10 iterations.\n");
 	return 0;
 }

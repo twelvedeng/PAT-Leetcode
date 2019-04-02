f3aa318a5367708afbd09ca260cc9f9fe979955f
twelvedeng <ddxy399@outlook.com>
2019-04-02 20:12:40 +0800

    -

---
+++ Leetcode/083 .py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None :
+            return head
+        p = head
+        while p.next is not None :
+            if p.val == p.next.val:
+                p.next = p.next.next
+            else :
+                p = p.next
+        return head

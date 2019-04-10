46e6c1580f5b0044efd1c1ee97434227d4d989d8
twelveD <ddxy399@outlook.com>
2019-04-10 16:38:10 +0800

    fix a :bug:

--- PAT_Advanced/1052 Linked List Sorting.cpp
+++ PAT_Advanced/1052 Linked List Sorting.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 struct node{
 	int ads, key, next;
-}result[100005];
-//node result[100005];
+}list[100005];
+node result[100005];
 bool cmp(node a, node b) {
 	return a.key < b.key;
 }
@@ -13,18 +13,17 @@ int main() {
 	scanf("%d %d", &n, &head);
 	for(int i=0; i < n; i++) {
 		scanf("%d", &temp);
-		scanf("%d%d", &result[i].key, &result[i].next);
-		result[i].ads = temp;
+		scanf("%d%d", &list[i].key, &list[i].next);
+		list[i].ads = temp;
 	}
-/*	int cnt = 0;
+	int cnt = 0;
 	for( ; head != -1; head = list[head].next)
 		result[cnt++] = list[head];
-*/	
+
 	sort(result, result+n, cmp);
 	printf("%d %05d\n", n, result[0].ads);
-	for(int i=0; i < n-1; i++)
+	for(int i=0; i < cnt-1; i++)
 		printf("%05d %d %05d\n", result[i].ads, result[i].key, result[i+1].ads);
-	printf("%05d %d -1\n", result[n-1].ads, result[n-1].key);
+	printf("%05d %d -1\n", result[cnt-1].ads, result[cnt-1].key);
 	return 0;
 }
-//21` ²âÊÔµã1¡¢4´ð°¸´íÎó 

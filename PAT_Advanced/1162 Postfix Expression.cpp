b30f67ce636dafdc7049fa7461910ca060e342cc
twelveD <ddxy399@outlook.com>
2019-09-11 17:11:30 +0800

    -

--- PAT_Advanced/1162 Postfix Expression.cpp
+++ PAT_Advanced/1162 Postfix Expression.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-struct node{
+struct node {
 	string data;
-	int left, right;
-};
+	int l, r;
+}a[100];
+string dfs(int root) {
+	if(a[root].l == -1 && a[root].r == -1)	return "("+a[root].data+")";
+	if(a[root].l == -1 && a[root].r != -1)	return "("+a[root].data+dfs(a[root].r)+")";
+	if(a[root].l != -1 && a[root].r != -1)	return "("+dfs(a[root].l)+dfs(a[root].r)+a[root].data+")";
+}
 int main() {
-	int n;
-	scanf("%d"£¬ &n);
-	vector<node> v(n+1);
-	string data;
-	int left, right;
+	int have[100] = {0}, n, root = 1;
+	cin >> n;
 	for (int i = 1; i <= n; i++) {
-		cin >> data >> left >> right;
-		v[i].push_back(node{data, left, right});
+		cin >> a[i].data >> a[i].l >> a[i].r;
+		if(a[i].l != -1) have[a[i].l] = 1;
+		if(a[i].r != -1) have[a[i].r] = 1;
 	}
-	
+	while(have[root] == 1) root++;
+	string ans = dfs(root);
+//	if(ans[0] == '(')	ans = ans.substr(1, ans.size()-2);
+	cout << ans;
 	return 0;
 }

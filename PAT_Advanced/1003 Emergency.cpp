1bbacea835695e127dac55671d7e765408ed0dcf
twelveD <ddxy399@outlook.com>
2019-09-26 20:04:50 +0800

    -

--- PAT_Advanced/1003 Emergency.cpp
+++ PAT_Advanced/1003 Emergency.cpp
@@ -1,44 +1,53 @@
-//Dijkstra
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+const int inf = 0x3fffffff;
+const int maxn = 502;
 int n, m, c1, c2;
-//e: the length between two cities
-//dist: sum of length from c1 to other city
-//num: numbers of short path
-//w: sum of cities' wight
-//weight: every city's weight
-int e[510][510], dist[510], num[510], w[510], weight[510];
-bool visit[510];
-const int inf = 99999999;
-using namespcae std;
-int main() {
-	cin >> n >> m >> c1 >> c2;
-	for (int i = 0; i < n; i++) 
-		cin >> weight[i];
-	//initialization array e and dist to positive infinity
-	fill(e[0], e[0]+510*510, inf);
-	fill(dist, dist+510, inf);
-	int a, b, c;
-	for (int i = 0; i < m; i++) {
-		//two cities' name and length between
-		cin >> a >> b >> c;
-		e[a][b] = e[b][a] = c;
-	}
-	dist[c1] = 0;	//the source's length
+int e[maxn][maxn], weight[maxn], w[maxn], d[maxn], cnt[maxn];
+bool vis[maxn] = {false};
+
+void dijkstra(int c1, int c2) {
+	fill(d, d+maxn, inf);
+	d[c1] = 0;
 	w[c1] = weight[c1];
-	num[c1] = 1;
-	
-	for (int i = 0; i < n; i++) {
-		int u = -1, minn = inf;
-		for (int j = 0; j < n; j++) {
-			if(visit[j] == false && dist[j] < minn) {
+	cnt[c1] = 1;
+	for(int i = 0; i < n; i++) {
+		int u = -1, mini = inf;
+		for(int j = 0; j < n; j++) {
+			if(!vis[j] && d[j] < mini) {
 				u = j;
-				minn = dis[j];
+				mini = d[j];
 			}
 		}
-		if(u == -1)	break;
+		if(u == -1) return;
+		vis[u] = true;
+		for(int v = 0; v < n; v++) {
+			if(!vis[v] && e[u][v] != inf) {
+				if(e[u][v]+d[u] < d[v]) {
+					d[v] = e[u][v] + d[u];
+					w[v] = weight[v] + w[u];
+					cnt[v] = cnt[u];
+				}else if(e[u][v]+d[u] == d[v]) {
+					cnt[v] += cnt[u];
+					if(weight[v]+w[u] > w[v])
+						w[v] = weight[v]+w[u];
+				}
+			}
+		}
 	}
+	printf("%d %d", cnt[c2], w[c2]);
+}
+
+int main() {
+	fill(e[0], e[0]+maxn*maxn, inf);
+	scanf("%d%d%d%d", &n, &m, &c1, &c2);
+	for(int i = 0; i < n; i++)
+		scanf("%d", &weight[i]);
+	int a, b, power;
+	for(int i = 0; i < m; i++) {
+		scanf("%d%d%d", &a, &b, &power);
+		e[a][b] = e[b][a] = power;
+	}
+	dijkstra(c1, c2);
 	return 0;
-	
 }

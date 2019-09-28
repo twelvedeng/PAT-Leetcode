7a4e496eb9fc6dc01bd3b65f7c7809daa58e95e1
twelveD <ddxy399@outlook.com>
2019-09-28 21:11:52 +0800

    -

--- PAT_Advanced/1160 Forever.cpp
+++ PAT_Advanced/1160 Forever.cpp
@@ -1,32 +1,47 @@
-#include <iostream>
-#include <cstring>
-#include <cmath>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 bool isprime(int n) {
 	if(n <= 1) return false;
-	for (int i = 0; i*i <= n; i++)
+	int sqt = int(sqrt(n*1.0));
+	for (int i = 2; i <= sqt; i++)
 		if(n%i == 0) return false;
 	return true;
-} 
-void judge(string ans, int m) {
-	string str = to_string(stoi(ans) + 1);
-	int n = 0;
-	for (int i = 0; i < str.size(); i++)
-		n += (str[i] - '0');
-	int x = 3;
-	while(++x && x < min(n, m)) {
-		if(n%x == 0 && m%x == 0 && isprime(x)) {
-			printf("%d %d\n", n, stoi(ans));
-			break;
-		}
-	}
-	//如果要排序ivi哈哈那就刺激还要写cmp还要存vector 
 }
+int sum(int n) {
+	int num = 0;
+	while(n != 0) {
+		num += n%10;
+		n /= 10;
+	}
+	return num;
+}
+/*int gcd(int a, int b) {
+	return a == 0 ? b : gcd(b%a, a);
+}*/
 int main() {
-	int n, k, m;
-	scanf("%d%d%d\n", &n, &k, &m);
-	
+	int N, k, m;
+	scanf("%d\n", &N);
+	for(int i = 1; i <= N; i++) {
+		scanf("%d %d", &k, &m);
+		printf("Case %d\n", i);
+		if(9*k < m) {
+			printf("No Solution\n");
+			continue;
+		}
+		bool flag = false;
+		for(int j = pow(10, k-1); j < pow(10, k); j++) {
+			if(j%9 != 0) continue;	//?
+			//int num = sum(j);
+			if(sum(j) == m) {
+				int n = sum(j+1);
+				int g = __gcd(n, m);
+				if(isprime(g) && g > 2) {
+					flag = true;
+					printf("%d %d\n", n, j);
+				}
+			}
+		}
+		if(flag == false) printf("No Solution\n");
+	}	
 	return 0;
 }
--- PAT_Advanced/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-	int digits[] = {0, 1, 1, 2, 2};
-	if(is_sorted(begin(digits), end(digits)))
-		cout << "Yes" << endl;
-	return 0;
-}
